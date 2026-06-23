8e89fca163118f6d1735e34e268cef09139fbd34
nahman <n0527610575@gmail.com>
2026-06-23 19:06:14 +0300

    file 1.1

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 class Animal:
     def __init__(self,legs,color,digsys,suns,race,colortail ):
         self.legs = legs
@@ -20,14 +21,22 @@ class Tail:
         self.thickness = thickness
         self.speed =speed
 class Cat(Animal):
-    def __init__(self,color):
-        super().__init__(legs=4,color=color,digsys="good",suns=6,race="italian",colortail="black")
+    def __init__(self,color,length_wih):
+
+        super().__init__(legs=4,color=color,length_wih=length_wih,digsys="good",suns=6,race="italian",colortail="black")
+        self.length_wih = length_wih
 cat_a = Cat(color="White")
 cat_b = Cat(color="Black")
 
 print(cat_a.legs)
 print(cat_a.color)
 print(cat_b.color)
+    def mice_hunted_get ():
+        random= random.randint(1, 10)
+        return random
+
+
+
 
 
 
