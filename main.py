96dba42d5998470eb63cd1f73e8aec1987afc3b1
nahman <n0527610575@gmail.com>
2026-06-24 07:48:59 +0300

    1.2

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ class Tail:
         self.length = length
         self.thickness = thickness
         self.speed =speed
+
 class Cat(Animal):
     def __init__(self,color,length_wih):
 
@@ -27,16 +28,19 @@ class Cat(Animal):
 
         super().__init__(legs=4,color=color,digsys="good",suns=6,race="italian",colortail="black")
         self.length_wih = length_wih
+        self.mice_counter = 0
+
+    def mice_hunted_get (self):
+        return self.mice_counter
+    def  mouse_hunt (self):
+        self.mice_counter += 1
+        print("החתול צד עכבר")
 cat_a = Cat(color="White",length_wih=10)
 cat_b = Cat( color="Black",length_wih= 15)
 
 print(cat_a.legs)
 print(cat_a.color)
 print(cat_b.color)
-def mice_hunted_get (self):
-
-    rand= random.randint(1, 10)
-    return rand
 
 
 
